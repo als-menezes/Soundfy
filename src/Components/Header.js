350d5f1bf4c02baa9d1e11553b5a716a6c4b0bd6
als-menezes <andre.sousamenezes2@gmail.com>
2024-12-14 14:47:23 -0300

    > Implement: Header navigations <

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -24,9 +26,20 @@ class Header extends Component {
       <header data-testid="header-component">
         {isLoading ? <Loading /> : null}
         <h2 data-testid="header-user-name">{ user }</h2>
+        <nav>
+          <Link data-testid="link-to-search" to="/search">Pesquisar</Link>
+          <Link data-testid="link-to-favorites" to="/favorites">Favoritos</Link>
+          <Link data-testid="link-to-profile" to="/profile">Perfil</Link>
+        </nav>
       </header>
     );
   }
 }
+
+Header.propTypes = {
+  match: PropTypes.objectOf(
+    PropTypes.string,
+  ),
+}.isRequired;
 
 export default Header;
