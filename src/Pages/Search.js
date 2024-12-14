ba0b15347cb5b0e4559de5b505a83d896e9cafa1
als-menezes <andre.sousamenezes2@gmail.com>
2024-12-14 15:38:43 -0300

    > Implement: search results <

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Header from '../Components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../Components/Loading';
 
 class Search extends Component {
   state = {
     searchInput: '',
+    isLoading: false,
+    searchResults: [],
+    artist: '',
+  };
+
+  handleSearchButton = async () => {
+    const { searchInput } = this.state;
+    this.setState({
+      artist: searchInput,
+      searchInput: '',
+      isLoading: true });
+    const results = await searchAlbumsAPI(searchInput);
+    this.setState({ searchResults: results, isLoading: false });
   };
 
   render() {
-    const { searchInput } = this.state;
+    const { searchInput, isLoading, searchResults, artist } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
@@ -19,14 +35,39 @@ class Search extends Component {
             value={ searchInput }
             onChange={ ({ target }) => this.setState({ searchInput: target.value }) }
           />
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ searchInput.length < 2 ? true : null }
-          >
-            Pesquisar
-          </button>
+
+          { isLoading
+            ? <Loading />
+            : (
+              <button
+                type="button"
+                data-testid="search-artist-button"
+                disabled={ searchInput.length < 2 ? true : null }
+                onClick={ this.handleSearchButton }
+              >
+                Pesquisar
+              </button>
+            ) }
         </form>
+        {searchResults && searchResults.length > 0
+          ? (
+            <>
+              <h1>{`Resultado de álbuns de: ${artist}`}</h1>
+              {searchResults.map((band, index) => (
+                <Link
+                  key={ index }
+                  to={ `/album/${band.collectionId}` }
+                  data-testid={ `link-to-album-${band.collectionId}` }
+                >
+                  <img src={ band.artworkUrl100 } alt={ band.collectionName } />
+                  <h2>{band.collectionName}</h2>
+                  <h3>{band.artistName}</h3>
+
+                </Link>
+              ))}
+            </>
+          )
+          : <h1>Nenhum álbum foi encontrado</h1>}
       </div>
     );
   }
